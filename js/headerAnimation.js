75e19739d845a6cad39af3d1c5f9cca46badb4ae
LeMustache <45916929+LeMoustache@users.noreply.github.com>
2023-01-23 09:06:28 +0100

    Update headerAnimation.js

--- js/headerAnimation.js
+++ js/headerAnimation.js
@@ -3,6 +3,7 @@ const logoText = document.querySelector("#logoText")
 
 const aboutLink = document.querySelector("#aboutLink")
 const projectLink = document.querySelector("#projectLink")
+const blogLink = document.querySelector("#blogLink")
 const contactLink = document.querySelector("#contactLink")
 
 const dropbtn = document.querySelector('#dropbtn')
@@ -14,6 +15,7 @@ tl.fromTo(logo,2,{x:'100%'},{x:'0%',ease:Power2.easeOut})
 .fromTo(logoText,2,{x:'-50%',opacity:'0'},{x:'0%',opacity:'1',ease:Power2.easeOut},"-=2")
 .from(aboutLink,1.5,{y:'-50',opacity:'0',ease:Power4.easeOut},"-=2")
 .from(projectLink,1.5,{y:'-50',opacity:'0',ease:Power4.easeOut},"-=1.7")
-.from(contactLink,1.5,{y:'-50',opacity:'0',ease:Power4.easeOut},"-=1.4")
+.from(blogLink,1.5,{y:'-50',opacity:'0',ease:Power4.easeOut},"-=1.4")
+.from(contactLink,1.5,{y:'-50',opacity:'0',ease:Power4.easeOut},"-=1.1")
 .from(dropbtn,1.5,{y:'-50',opacity:'0',ease:Power4.easeOut},"-=2.5")
 
