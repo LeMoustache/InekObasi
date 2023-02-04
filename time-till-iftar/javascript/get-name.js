f1afe4e08d9263e6cefc6e5d607d2ba166024092
LeMustache <45916929+LeMoustache@users.noreply.github.com>
2023-02-04 09:00:28 +0100

    Update get-name.js

--- time-till-iftar/javascript/get-name.js
+++ time-till-iftar/javascript/get-name.js
@@ -1,12 +1,11 @@
-$.getJSON("https://lemustache.net/time-till-iftar/database/names-sa.json",function(data){ // get arabic name
-
-    dataInd = Math.floor(Math.random() * data.length)
-
-    document.getElementById("nameTitleArb").innerHTML = data[dataInd].name
-})
-
 function translateName(){
     lang = document.querySelector('input[name="lang"]:checked').value; // get translated name
+
+    $.getJSON("https://lemustache.net/time-till-iftar/database/names-sa.json",function(data){ // get arabic name
+        dataInd = Math.floor(Math.random() * data.length)
+
+        document.getElementById("nameTitleArb").innerHTML = data[dataInd].name
+    })
 
     $.getJSON("https://lemustache.net/time-till-iftar/database/names-"+lang+".json",function(data){
         document.getElementById("nameTitle").innerHTML = data[dataInd].index+" "+data[dataInd].name
